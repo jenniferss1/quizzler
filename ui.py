7351a6fdaee232d5673d52842a4c07fc651ca5c9
jennifer <jennifersantossouza@hotmail.com>
2025-05-24 11:55:14 -0300

    feat: creating the canvas interface

--- ui.py
+++ ui.py
@@ -8,25 +8,40 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         
-        self.create_true_button()
-        self.create_false_button()
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canva()
+        self.true_button()
+        self.false_button()
         
         self.window.mainloop()
+  
+    def canva(self):
+        self.canvas = Canvas(width=300, height=250)
+        self.canvas.create_text(
+            150, 
+            125, 
+            text="A pergunta vai aqui", 
+            fill=THEME_COLOR, 
+            font=("Ariel", 20, "italic")
+            )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
     
-    def create_true_button(self):
+    def true_button(self):
         self.true_image = PhotoImage(file="images/true.png")
         self.true_button = Button(
             image=self.true_image, 
             highlightthickness=0, 
             borderwidth=0
         )
-        self.true_button.grid(row=0, column=0)
+        self.true_button.grid(row=2, column=0)
     
-    def create_false_button(self):
+    def false_button(self):
         self.false_image = PhotoImage(file="images/false.png")
         self.false_button = Button(
             image= self.false_image,
             highlightthickness=0,
             borderwidth=0
         )
-        self.false_button.grid(row=0, column=1)
+        self.false_button.grid(row=2, column=1)
